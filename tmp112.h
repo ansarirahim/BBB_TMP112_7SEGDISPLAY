54cca485261042315951bf6c054bcc3a13001e2a
AbdulRaheem <ansarirahim1@gmail.com>
2022-04-20 08:17:06 +0530

    header file for both 7 segment and bash

---
+++ tmp112.h
@@ -0,0 +1,10 @@
+#ifndef __TEMP112_H__
+#define __TEMP112_H__
+#define CALLIB_FILE "/root/Tmp112/callib"
+#define MYBUFSIZE 100
+#define COMMAND_DISPLAY7SEGSTRING  "/root/Tmp112/Display7SegmentString "
+
+#define COMMAND_GET_TEMP_CALIB_DATA "GET_TEMP_CALIB_DATA"
+#define ENABLE_UART4 "echo BB-UART4 > /sys/devices/bone_capemgr.9/slots"
+#endif
+
